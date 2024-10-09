80599c5c797acc7e48ca128be139f00627b9f65f
Berhan <berhan92gmail.com>
2024-10-08 22:27:43 -0400

    css updated

--- FrontEnd/Client/src/Components/SignUp/SignUp.jsx
+++ FrontEnd/Client/src/Components/SignUp/SignUp.jsx
@@ -1,9 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import axios from "../../axiosConfig";
 import { Link, useNavigate } from "react-router-dom";
 import "./signup.css"; // Import the CSS file
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
   const navigate = useNavigate();
   const userNameDom = useRef(null);
   const fistNameDom = useRef(null);
@@ -43,47 +46,67 @@ const SignUp = () => {
       console.log(error.response);
     }
   }
+  // Toggles the password visibility between plain text and hidden
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className="SignUpContainer">
-      <div className="register-form-container">
-        <h2 className="header">Join the network</h2>
-        <p className="firstAlreadyhaveAcc">
-          Already have an account?{" "}
-          <Link to="/login" className="login-link">
-            Sign in
-          </Link>
-        </p>
-        <form className="SignupForm" action="" onSubmit={handleSubmit}>
-          <div>
-            <input ref={userNameDom} type="text" placeholder="Username" />
-          </div>
-          <div className="fullName">
-            <input ref={fistNameDom} type="text" placeholder="First name" />
-            <input ref={lastNameDom} type="text" placeholder="Last name" />
-          </div>
-          <div>
-            <input ref={emailDom} type="email" placeholder="Email address" />
-          </div>
-          <div>
-            <input ref={passwordDom} type="password" placeholder="Password" />
-          </div>
-          <div>
-            <small>
-              {" "}
-              I agree to the <Link to="">privacy policy</Link> and{" "}
-              <Link to="">terms of service</Link>
-            </small>
-          </div>
-          <button className="btnJoin" type="submit">
-            Agree and Join
-          </button>
-
-          <Link to="/login" className="login-link">
-            <p className="already-account">Already have an account? </p>
-          </Link>
-        </form>
+      <div className="register-form-wrapper">
+        {/* Left-side form section */}
+        <div className="register-form-container">
+          <h2 className="header">Join the network</h2>
+          <p className="firstAlreadyhaveAcc">
+            Already have an account?{" "}
+            <Link
+              to="/SignIn"
+              className="login-link"
+              style={{ color: "#ff8401" }}
+            >
+              Sign in
+            </Link>
+          </p>
+          <form className="SignupForm" action="" onSubmit={handleSubmit}>
+            <div>
+              <input ref={emailDom} type="email" placeholder="Email" />
+            </div>
+            <div className="fullName">
+              <input ref={fistNameDom} type="text" placeholder="First Name" />
+              <input ref={lastNameDom} type="text" placeholder="Last Name" />
+            </div>
+            <div>
+              <input ref={userNameDom} type="text" placeholder="User Name" />
+            </div>
+            <div>
+              <input ref={passwordDom} type="password" placeholder="Password" />
+              <span
+                className="visible-password"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}{" "}
+                {/* Toggle visibility icon */}
+              </span>
+            </div>
+            <div>
+              <small>
+                I agree to the{" "}
+                <Link to="" style={{ color: "#ff8401" }}>
+                  privacy policy
+                </Link>{" "}
+                and{" "}
+                <Link to="" style={{ color: "#ff8401" }}>
+                  terms of service
+                </Link>
+              </small>
+            </div>
+            <button className="btnJoin" type="submit">
+              Agree and Join
+            </button>
+          </form>
+        </div>
       </div>
+
       {/* Right-side info section */}
       <div className="info-section">
         <h2>Evangadi Networks Q & A</h2>
