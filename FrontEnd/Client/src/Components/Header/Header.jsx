177067246e596d8b0bb20ef3c3dc09cbacc24968
Berhan <berhan92gmail.com>
2024-10-10 10:08:25 -0400

    header and footer edited

--- FrontEnd/Client/src/Components/Header/Header.jsx
+++ FrontEnd/Client/src/Components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 import "./header.css";
 
 const Header = () => {
   return (
     <header className="header">
+      {/* Logo section, linking back to the homepage */}
       <div className="logo">
         <Link to="/">
           <img
@@ -13,15 +14,15 @@ const Header = () => {
           />
         </Link>
       </div>
+
+      {/* Navigation section with links to different pages */}
       <nav className="nav-links">
-        <Link to="/">Home</Link>
-
-        <Link to="/HowItWorks">How it Works</Link>
-        <Link to="/login" className="login-btn">
-
-        <Link to="/how-it-works">How it Works</Link>
+        <Link to="/">Home</Link> {/* Link to Home page */}
+        <Link to="/HowItWorks">How it Works</Link>{" "}
+        {/* Link to How it Works page */}
         <Link to="/SignIn" className="login-btn">
-
+          {" "}
+          {/* Link to Sign In page */}
           SIGN IN
         </Link>
       </nav>
