ee9f8bab8784362b71176d5d50aef160833be1b0
Mimidemissie <164256332+Mimidemissie@users.noreply.github.com>
2024-10-11 08:13:46 -0400

    update signUp css

--- FrontEnd/Client/src/Components/SignUp/SignUp.jsx
+++ FrontEnd/Client/src/Components/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import axios from "../../axiosConfig";
 import { Link, useNavigate } from "react-router-dom";
-import "./SignUp";
+import "./signup.css";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
